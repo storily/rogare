33139255ddfdfd288a246d7c948070ea55547dfb
Félix Saparelli <felix@passcod.name>
2017-11-16 12:20:22 +1300

    Declare custom plugin lists up front

--- lib/rogare.rb
+++ lib/rogare.rb
@@ -48,8 +48,11 @@ module Rogare
   end
 
   module Plugins
+    @@custom_configs = []
+    @@custom_plugins = []
+
     def self.to_a
-      self.constants.map { |c| self.const_get c } + (@@custom_plugins || [])
+      self.constants.map { |c| self.const_get c } + @@custom_plugins
     end
 
     def self.config(c)
@@ -59,9 +62,7 @@ module Rogare
     end
 
     def self.add_plugin(const, &block)
-      @@custom_plugins ||= []
       @@custom_plugins << const
-      @@custom_configs ||= []
       @@custom_configs << block
     end
   end
