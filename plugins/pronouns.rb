25765f670d38eabfcb793cc96e4bb652f2a146c2
Félix Saparelli <felix@passcod.name>
2018-11-01 00:49:49 +1300

    Add pronouns

---
+++ plugins/pronouns.rb
@@ -0,0 +1,13 @@
+class Rogare::Plugins::Pronouns
+  include Cinch::Plugin
+  extend Rogare::Help
+
+  command 'pronouns', hidden: true
+  usage '!% - You\'re a sweetie'
+  handle_help
+
+  match_empty :execute
+  def execute(m)
+    m.reply "My pronouns are it/they, thanks for asking :)"
+  end
+end
