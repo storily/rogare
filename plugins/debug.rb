89a219003df16e614ad3bb361e0d1c4e4eea20db
Félix Saparelli <felix@passcod.name>
2018-11-03 15:31:20 +1300

    Add voice play debug

--- plugins/debug.rb
+++ plugins/debug.rb
@@ -16,6 +16,7 @@ class Rogare::Plugins::Debug
   match_command /war mems (.+)/, method: :war_mems
   match_command /voice connect (.+)/, method: :voice_connect
   match_command /voice on/, method: :voice_on
+  match_command /voice play (.+)/, method: :voice_play
   match_command /voice off/, method: :voice_off
   match_command /voice bye/, method: :voice_bye
 
@@ -90,6 +91,38 @@ class Rogare::Plugins::Debug
 
   def voice_on(m)
     Rogare.discord.voice(m.channel.server).speaking = true
+  end
+
+  def voice_play(m, name)
+    return if name.include? '/'
+
+    file = Dir["./voice/#{name}.dca"].first
+    file ||= Dir["./voice/#{name}.mp3"].first
+    return m.reply 'No such file' unless file
+
+    voice = Rogare.discord.voice(m.channel.server)
+    voice.speaking = true
+
+    begin
+      m.reply 'Playing'
+      logs 'What'
+      if file.end_with? '.dca'
+        logs 'Dca'
+        voice.play_dca(file)
+      else
+        logs 'File'
+        logs file.inspect
+        voice.play_io(open(file))
+      end
+      logs 'What'
+      m.reply 'Played'
+    rescue StandardError => e
+      logs e.message
+      logs e.backtrace
+      m.reply "Error playing #{name}"
+    end
+
+    voice.speaking = false
   end
 
   def voice_off(m)
