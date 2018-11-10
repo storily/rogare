5a6c14d28bcfcd2b8a7cf01615408d5e8de53603
Félix Saparelli <felix@passcod.name>
2018-11-11 01:22:05 +1300

    Show more for debug info name

--- plugins/debug.rb
+++ plugins/debug.rb
@@ -100,7 +100,8 @@ class Rogare::Plugins::Debug
 
   def name_info(m, name)
     m.debugly(
-      Rogare::Data.names.where(name: name.downcase).first,
+      *Rogare::Data.names.where(name: name.downcase).all,
+      '------------------------------------------------',
       *Rogare.sql[:names].where(name: name.downcase).select(:name, :kinds, :source, :surname).distinct.all
     )
   end
