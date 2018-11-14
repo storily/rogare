67a295daec3407e226ac81484fe4f71fd14b0912
Félix Saparelli <felix@passcod.name>
2018-11-15 00:27:00 +1300

    Reconfigure bot.rb to avoid having dev modifications in there all the time

--- bot.rb
+++ bot.rb
@@ -14,6 +14,11 @@ Rogare.sql
 
 logs '=====> Loading modules'
 Dir['./plugins/*.rb'].each do |p|
+  name = Pathname.new(p).basename('.rb').to_s
+  if ENV['MODULES_WHITELIST']
+    next unless ENV['MODULES_WHITELIST'].split(',').include? name
+  end
+
   require p
 end
 
@@ -21,12 +26,14 @@ logs '=====> Preparing threads'
 require 'thwait'
 threads = []
 
-threads << Thread.new do
-  sleep 3
-  logs '=====> Loading wordwars from Redis'
-  wars = Rogare::Plugins::Wordwar.load_existing_wars
-  logs "=====> Loaded #{wars.count} wordwars, now waiting on timers"
-  wars.each(&:join)
+if ENV['RACK_ENV'] == 'production' || ENV['DEV_LOAD_WARS']
+  threads << Thread.new do
+    sleep 3
+    logs '=====> Loading wordwars from Redis'
+    wars = Rogare::Plugins::Wordwar.load_existing_wars
+    logs "=====> Loaded #{wars.count} wordwars, now waiting on timers"
+    wars.each(&:join)
+  end
 end
 
 if ENV['RACK_ENV'] == 'production'
