99196332c09c24846308d43de17b4a48bc944b9a
Félix Saparelli <felix@passcod.name>
2018-11-18 23:48:01 +1300

    Add special super-shorthand !w

---
+++ plugins/w.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Rogare::Plugins::W
+  extend Rogare::Plugin
+
+  command 'w', hidden: true
+  match_empty :execute
+
+  def execute(m, _param)
+    Rogare::Plugins::Wordcount.new.own_count(m) if defined? Rogare::Plugins::Wordcount
+    Rogare::Plugins::Wordwar.new.ex_list_wars(m) if defined? Rogare::Plugins::Wordwar
+  end
+end
