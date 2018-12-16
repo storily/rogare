3c7280baf2ca5d6d40cc9db548c53f9880998622
Félix Saparelli <felix@passcod.name>
2018-12-16 13:22:34 +1300

    Load a bunch of Sequel model plugins

--- cli.rb
+++ cli.rb
@@ -12,6 +12,10 @@ require './lib/rogare'
 logs '=====> Preparing resources'
 DB = Rogare.sql
 Rogare::Data.goal_parser_impl
+Sequel::Model.plugin :eager_each
+Sequel::Model.plugin :pg_auto_constraint_validations
+Sequel::Model.plugin :prepared_statements
+Sequel::Model.plugin :prepared_statements_safe
 
 logs '=====> Loading models'
 Dir['./models/*.rb'].each do |p|
