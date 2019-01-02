056e202396497cac94c9949b56b2c60e79f8d0d1
Félix Saparelli <felix@passcod.name>
2019-01-02 22:38:34 +1300

    [migration] Rewrite fkeys

--- migrations/20190101_zz_foreign_keys.rb
+++ migrations/20190101_zz_foreign_keys.rb
@@ -1,26 +1,53 @@
 # frozen_string_literal: true
 
 Sequel.migration do
-  change do
-    # ALTER TABLE ONLY goals
-    # ADD CONSTRAINT goals_novel_id_fkey FOREIGN KEY (novel_id) REFERENCES novels(id);
-    # ALTER TABLE ONLY goals
-    # ADD CONSTRAINT goals_parent_id_fkey FOREIGN KEY (parent_id) REFERENCES goals(id);
-    #
-    # ALTER TABLE ONLY novels
-    # ADD CONSTRAINT nanos_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
-    #
-    # ALTER TABLE ONLY wars_members
-    # ADD CONSTRAINT wars_members_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
-    # ALTER TABLE ONLY wars_members
-    # ADD CONSTRAINT wars_members_war_id_fkey FOREIGN KEY (war_id) REFERENCES wars(id);
-    #
-    # ALTER TABLE ONLY wars
-    # ADD CONSTRAINT wars_canceller_fkey FOREIGN KEY (canceller_id) REFERENCES users(id);
-    # ALTER TABLE ONLY wars
-    # ADD CONSTRAINT wars_creator_fkey FOREIGN KEY (creator_id) REFERENCES users(id);
-    #
-    # ALTER TABLE ONLY wordcounts
-    # ADD CONSTRAINT wordcounts_novel_id_fkey FOREIGN KEY (novel_id) REFERENCES novels(id);
+  up do
+    alter_table(:goals) do
+      add_foreign_key [:novel_id], :novels
+      add_foreign_key [:parent_id], :goals
+    end
+
+    alter_table(:novels) do
+      add_foreign_key [:user_id], :users
+    end
+
+    alter_table(:wars_members) do
+      add_foreign_key [:user_id], :users
+      add_foreign_key [:war_id], :wars
+    end
+
+    alter_table(:wars) do
+      add_foreign_key [:canceller_id], :users
+      add_foreign_key [:creator_id], :users
+    end
+
+    alter_table(:wordcounts) do
+      add_foreign_key [:novel_id], :novels
+    end
+  end
+
+  down do
+    alter_table(:goals) do
+      drop_foreign_key [:novel_id]
+      drop_foreign_key [:parent_id]
+    end
+
+    alter_table(:novels) do
+      drop_foreign_key [:user_id]
+    end
+
+    alter_table(:wars_members) do
+      drop_foreign_key [:user_id]
+      drop_foreign_key [:war_id]
+    end
+
+    alter_table(:wars) do
+      drop_foreign_key [:canceller_id]
+      drop_foreign_key [:creator_id]
+    end
+
+    alter_table(:wordcounts) do
+      drop_foreign_key [:novel_id]
+    end
   end
 end
